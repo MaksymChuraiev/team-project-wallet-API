630926c69e64afa35c77646a7fbbb7bf60dba20c
Yevheniia Ukolova <ukolova_e@ukr.net>
2022-07-30 22:50:57 +0300

    owner id added

--- controllers/transactions/create.js
+++ controllers/transactions/create.js
@@ -1,7 +1,8 @@
 const { Transaction } = require("../../models");
 
 const create = async (req, res) => {
-  const newTransaction = await Transaction.create({ ...req.body });
+  const { _id } = req.user;
+  const newTransaction = await Transaction.create({ ...req.body, owner: _id });
   res.status(201).json({
     status: "success",
     code: 201,
--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 const { User, signupJoiSchema, signinJoiSchema } = require("./user");
+const { Transaction, joiCreateSchema } = require("./transaction");
 
 module.exports = {
   User,
   signupJoiSchema,
   signinJoiSchema,
+  Transaction,
+  joiCreateSchema,
 };
