7c19f68bc8618daf7d6a34c3548102f31f1a761c
Yevheniia Ukolova <ukolova_e@ukr.net>
2022-07-30 17:31:34 +0300

    signin controller added

--- controllers/auth/signin.js
+++ controllers/auth/signin.js
@@ -1,3 +1,26 @@
-const signin = async (req, res) => {};
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+const { Unauthorized } = require("http-errors");
+
+const { SECRET_KEY } = process.env;
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !user.comparePassword(password)) {
+    throw new Unauthorized(`Email or password is wrong`);
+  }
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+  res.json({
+    status: "Success",
+    code: 200,
+    data: {
+      token,
+    },
+  });
+};
 
 module.exports = signin;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,7 @@
 const auth = require("./auth");
 const transactions = require("./transactions");
 const users = require("./users");
+
 module.exports = {
   auth,
   transactions,
